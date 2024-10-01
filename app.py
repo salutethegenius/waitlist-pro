f2fe6ed8abee0ac81512b6b7f0e6e4b71fdce828
salutethegenius <36910130-salutethegenius@users.noreply.replit.com>
2024-10-01 11:03:29 +0000

    Agent query: Can you register a new participant and check if the registration is successful without any email confirmation step?

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from flask_mail import Mail
 from extensions import db, app
 from models import Participant
 from sqlalchemy import inspect
@@ -7,16 +6,7 @@ from sqlalchemy import inspect
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "a_secure_secret_key")
 
-# Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
-
 db.init_app(app)
-mail = Mail(app)
 
 with app.app_context():
     db.create_all()
