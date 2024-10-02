9f8eaab4db562409f5e7ebb7b99fdb0a01c05f9a
Kenneth Moncur <36910130-salutethegenius@users.noreply.replit.com>
2024-10-02 05:02:25 +0000

    Initial commit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     logger.info("Starting Flask application")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
